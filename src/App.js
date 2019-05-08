5157bbbcd2a26bd39f7fc986368562e3cd7c4e8e
hansnow <hansnow2012@gmail.com>
2019-05-08 10:36:51 +0800

    feat: add react-router as routing library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'
+import './App.css'
+
+import Home from './Home'
+import Performance from './cases/performance'
+
+function SidebarLink(props) {
+  return <NavLink activeClassName="active" {...props} />
+}
 
 function App() {
-  return <div>Hello</div>
+  return (
+    <Router>
+      <div class="container">
+        <div class="sidebar">
+          <SidebarLink exact to="/">
+            Home
+          </SidebarLink>
+          <SidebarLink to="/performance">表单性能问题</SidebarLink>
+        </div>
+        <div class="main">
+          <Route exact path="/" component={Home} />
+          <Route path="/performance" component={Performance} />
+        </div>
+      </div>
+    </Router>
+  )
 }
 
 export default App
---
+++ src/cases/performance/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Performance() {
+  return <h1>rc-form性能问题</h1>
+}
